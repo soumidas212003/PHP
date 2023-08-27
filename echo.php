6767c5dd96dfb938f47da62122db88cd480833c6
Soumita Das <soumitad991@gmail.com>
2023-08-27 12:30:48 +0530

    Print message

---
+++ echo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //echo "Soumita Das";
+        //echo 'Soumita Das';
+        //echo ("Soumita Das");
+        //echo ('Soumita Das');
+        //echo "Soumita"," Soumi"; //two string
+        //echo "Soumita"." Das";//concatenate as one strings
+        //echo "<b><i>Soumita"." Das</i></b>";
+        //echo "<b><i>Soumita Das</i></b>";
+        //echo 45.12;
+        echo 10456;
+    ?>
+</body>
+</html>
