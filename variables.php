b2095dfe8f5e3e81267fd1d4e43a2cfc149b8071
Soumita Das <soumitad991@gmail.com>
2023-08-27 13:01:47 +0530

    Variables

---
+++ variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        /*$name = "Soumita Das<br>";
+        echo $name;
+        echo $name;
+        echo $name;*/
+        /*$name="Soumita Das";
+        echo "<h1>".$name."</h1>";
+        echo "<h2>".$name."</h2>";
+        echo "<h3>".$name."</h3>";*/
+        $name="Soumita Das";
+        $num= 20;
+        echo "What is your name ? <br> My name is ".$name."<br>";
+        echo $num;
+    ?>
+</body>
+</html>
